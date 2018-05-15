aa42e044b9a666ca7343f496cc0148e394499d98
Scipio Africanus <jljones657@gmail.com>
2018-05-15 18:23:12 -0500

    Slightly better UI

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,27 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  
   // Find all Authors and return them to the user with res.json
   app.get("/api/recipes", function(req, res) {
     db.Recipe.findAll({}).then(function(dbRecipe) {
       res.json(dbRecipe);
     });
   });
+
+    // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Recipe.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    })
+    .then(function(dbRecipe) {
+      res.json(dbRecipe);
+    });
+  });
+
+
+
 };
