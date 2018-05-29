810b0c20d264f3adc085ce666fd4db687e50e63b
Chitica <liseth_gdl_86@hotmail.com>
2018-05-29 09:01:35 -0500

    s

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,5 +1,6 @@
 
 "use strict";
+
 module.exports = function(sequelize, DataTypes) {
   var Ingredient = sequelize.define("Ingredient", {
       name: DataTypes.STRING,
@@ -16,3 +17,21 @@ module.exports = function(sequelize, DataTypes) {
 
   return Ingredient;
 };
+
+
+// module.exports = function(sequelize, DataTypes) {
+//   var Ingredient = sequelize.define("Ingredient", {
+//       name: DataTypes.STRING,
+//       type: DataTypes.STRING,
+//       // recipeId: DataTypes.INTEGER
+//   }, {
+//     // disable timestamps
+//     timestamps: false
+//   });
+
+//   Ingredient.associate = function(models){
+//     Ingredient.belongsTo(models.Recipe);
+//   };
+
+//   return Ingredient;
+// };
